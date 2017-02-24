bbab8ce2425b68243567c6bfaed566cddbd1fe52
wuhanrockseawmj <877057514@qq.com>
2017-02-24 11:07:02 +0800

    Initial commit

---
+++ app/src/main/java/com/android/wuhanrocksea/timetracker/MainActivity.java
@@ -0,0 +1,44 @@
+package com.android.wuhanrocksea.timetracker;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    class MyAsyncTask extends AsyncTask<Long,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Long... params) {
+            long startTime = params[0];
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+}
